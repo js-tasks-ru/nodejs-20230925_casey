a3d50614b6186be3007f57e2ecb26cee81c3d7d7
gitVera <sunnyvivi3@gmail.com>
2023-10-29 10:35:17 +0300

    api text search

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,13 @@
+const mapProduct = require('../mappers/product');
+const Product = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const {query} = ctx.query;
+  if (!query) return next();
+
+  const products = await Product
+      .find({$text: {$search: query}}, {score: {$meta: 'textScore'}})
+      .sort({score: {$meta: 'textScore'}});
+
+  ctx.body = {products: products.map(mapProduct)};
 };
