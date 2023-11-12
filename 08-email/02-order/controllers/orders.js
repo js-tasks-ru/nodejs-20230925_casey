00365c81d0beff69d30cc02c16cf7fd817ec552a
gitVera <sunnyvivi3@gmail.com>
2023-11-12 16:10:00 +0300

    создание заказа, получения списка заказов юзера

--- 08-email/02-order/controllers/orders.js
+++ 08-email/02-order/controllers/orders.js
@@ -1,8 +1,36 @@
+const Product = require('../models/Product');
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
+const mapOrder = require('../mappers/order');
+const mapOrderConfirmation = require('../mappers/orderConfirmation');
 
-module.exports.checkout = async function checkout(ctx, next) {
+module.exports.checkout = async function checkout(ctx, next) { // создание заказа
+  const user = ctx.user;
+
+  const order = new Order({
+    user: user, // наш юзер
+    product: ctx.request.body.product,
+    phone: ctx.request.body.phone,
+    address: ctx.request.body.address,
+  });
+  await order.save(); // ??
+
+  // find product by ObjectId from order scheme
+  const product = await Product.findById(order.product);
+
+  await sendMail({
+    to: user.email,
+    subject: 'Подтверждение создания заказа',
+    locals: mapOrderConfirmation(order, product),
+    template: 'order-confirmation',
+  });
+
+  ctx.body = {status: 'ok'};
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  // возвращаем заказы только нашего юзера
+  const orders = await Order.find({user: ctx.user});
+
+  ctx.body = orders.map((order) => mapOrder(order));
 };
