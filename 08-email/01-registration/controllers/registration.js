ae2f42619eebb33d77a1da3a1cd73d34a8b1ec81
gitVera <sunnyvivi3@gmail.com>
2023-11-12 15:44:51 +0300

    confirm reg by email

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -1,11 +1,44 @@
-const { v4: uuid } = require('uuid');
+const {v4: uuid} = require('uuid');
 const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const verificationToken = uuid();
+  const user = new User({
+    email: ctx.request.body.email,
+    displayName: ctx.request.body.displayName,
+    verificationToken,
+  });
 
+  await user.setPassword(ctx.request.body.password);
+  await user.save();
+
+  // отправляем токен для подтверждения
+  await sendMail({
+    to: user.email,
+    subject: 'Подтвердите почту',
+    locals: {token: verificationToken},
+    template: 'confirmation',
+  });
+
+  ctx.body = {status: 'ok'};
 };
 
+// при подтверждении из письма
 module.exports.confirm = async (ctx, next) => {
+  // ищем юзера по значению переданного токена
+  const user = await User.findOne({
+    verificationToken: ctx.request.body.verificationToken,
+  });
 
+  if (!user) {
+    ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+  }
+  // если юзер найден - удаляем токен подтверждения
+  user.verificationToken = undefined;
+  await user.save();
+
+  // аутентифицировать пользователя, сгенерировав для него новый ключ сесии (также, с помощью uuid)
+  const token = uuid();
+  ctx.body = {token};
 };
