dff5629434eb2047f1f720bc8f89fc9c4702e823
gitVera <sunnyvivi3@gmail.com>
2023-11-20 12:07:32 +0300

    websocket

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,8 @@
 const Message = require('../models/Message');
+const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+  // ищем сообщения по юзер id
+  const messages = await Message.find({chat: ctx.user.id}).sort({date: 1}).limit(20);
+  ctx.body = {messages: messages.map(mapMessage)};
 };
--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -7,11 +7,30 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    const token = socket.handshake.query.token;
+
+    if (!token) return next(new Error('anonymous sessions are not allowed'));
+    // по токену получаем сессию и текущего юзера
+    const session = await Session.findOne({token}).populate('user');
+
+    if (!session) return next(new Error('wrong or expired session token'));
+    // записываем в сокет текущего юзера
+    socket.user = session.user;
+
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      const date = new Date();
+      // при получении сообщения с веб-сокетов - записываем его в БД
+      await Message.create({
+        chat: socket.user.id,
+        user: socket.user.displayName,
+        text: msg,
+        date: date,
+      });
+    });
   });
 
   return io;
